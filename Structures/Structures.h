76d3bd6e9d285cec00c605ea5ad575c24091fee6
Michael Choe <michael@straussmade.com>
2016-01-29 11:12:06 -0500

    Initial Commit

---
+++ Structures/Structures.h
@@ -0,0 +1,19 @@
+//
+//  Structures.h
+//  Structures
+//
+//  Created by Michael Choe on 1/29/16.
+//  Copyright © 2016 Strauss LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Structures.
+FOUNDATION_EXPORT double StructuresVersionNumber;
+
+//! Project version string for Structures.
+FOUNDATION_EXPORT const unsigned char StructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Structures/PublicHeader.h>
+
+
